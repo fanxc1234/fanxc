d05acb0f893bbb3bcba76e197bd323a79c57a881
fanxc1234 <56380478+fanxc1234@users.noreply.github.com>
2020-06-05 10:13:41 +0800

    电梯

---
+++ 2020-05-26_14-04-50/algorithm_interface.py
@@ -0,0 +1,76 @@
+import json
+
+from json import JSONEncoder
+import make_json_serializable  # 用于自定义对象的JSON
+
+import algorithm_test
+# 关于楼层编号:从地下3到地上18编号 0-20 一楼编号:3
+
+max_person = 12  # 电梯载客量
+elevator_speed = 0.5  # 电梯速度,每秒0.5层
+NUMBER_OF_FLOOR_LEVELS = 21 #总楼层数
+
+
+    
+class person:  # 人
+    come_time = 0  # 出现的时间
+    from_floor = 0  # 出发楼层
+    to_floor = 0  # 到达楼层
+    current_floor = 0  # 现在的楼层
+    in_which_elevator = 0  # 在哪个电梯中,可取 0 1 2 3 分别指不在电梯中 在第一个电梯中 在第二个电梯中 在第三个电梯中
+    is_out = False  # 是否已经完成电梯乘坐
+
+    def to_json(self):
+        dict_name_value = {}
+
+        for name, value in vars(self).items():
+            dict_name_value[name] = value
+        return dict_name_value
+# 电梯
+# e1:只停单层，e2:只停双数层(包括1层)，e3:全停
+# 单:index=0 2 3 5 7 9 ..
+# 双:index=1 4 6 8 ..
+# index=3为F1,3个电梯都可以停
+
+class elevator:
+    current_floor = 3.0  # 现在的楼层,允许小数,每一秒都要进行精确更新.初始位置1楼
+    move_direction = 0  # 移动方向,可取到0 1 2,分别是 停止 向上 向下
+
+    def __init__(self, current_floor, move_direction):
+        self.current_floor = current_floor  
+        self.move_direction = move_direction
+
+    def to_json(self):
+        dict_name_value = {}
+
+        for name, value in vars(self).items():
+            dict_name_value[name] = value
+        return dict_name_value
+# 核心算法:顺向截停
+# 对输入人群数组,不应假设每个人的出现时间都是1 6 11 ... 而可能是任意整数时间点(不方便的话再进行沟通),不应假设come_time<=t
+
+
+def core_algorithm(time, array_people):
+    #初始化楼层对象(不太熟悉python语法，写的有点奇怪？
+    pass
+
+class algorithm_result:
+    elevators = []
+    people = []
+
+    def to_json(self):
+        dict_name_value = {}
+
+        for name, value in vars(self).items():
+            dict_name_value[name] = value
+        return dict_name_value
+
+
+# 通过标准输入输出,供外部程序调用,还没有写
+if __name__ == "__main__":
+    result = algorithm_test.test2()
+    a = algorithm_result()
+    a.elevators = result[0]
+    a.people = result[1]
+    print(json.dumps(a, indent=4))
+    # print(json.dumps(result, indent=4))
---
+++ 2020-05-26_14-04-50/algorithm_test.py
@@ -0,0 +1,231 @@
+from algorithm_interface import *
+import algorithm_implement
+import os
+import file_util
+# 测试算法
+
+
+def test1():
+    person_1 = person()
+    person_1.from_floor = 3
+    person_1.to_floor = 12
+    person_1.come_time = 1
+    person_1.current_floor = 3
+    person_1.in_which_elevator = 0
+    person_1.is_out = False
+
+    person_2 = person()
+    person_2.from_floor = 3
+    person_2.to_floor = 12
+    person_2.come_time = 1
+    person_2.current_floor = 3
+    person_2.in_which_elevator = 0
+    person_2.is_out = False
+
+    person_3 = person()
+    person_3.from_floor = 3
+    person_3.to_floor = 12
+    person_3.come_time = 1
+    person_3.current_floor = 3
+    person_3.in_which_elevator = 0
+    person_3.is_out = False
+
+    core_algorithm(7, [person_1, person_2, person_3])
+    # 有3个人在1.0秒出现于第3层(第0层指地下三层,第3层指1楼),要前往第12层,求第7.0秒时电梯和人员的位置
+
+    # 由第3层到第12层,只能使用电梯0
+
+    # 1.0秒到7.0秒,共6秒,电梯上移三层,由3层到6层(还没有考虑进入电梯的时间)
+
+    # 所以应该返回
+    person_1.current_floor = 6
+    person_1.in_which_elevator = 1
+    person_2.current_floor = 6
+    person_2.in_which_elevator = 1
+    person_3.current_floor = 6
+    person_3.in_which_elevator = 1
+
+    return [[elevator(6, 1), elevator(3, 0), elevator(3, 0)], [person_1, person_2, person_3]]
+
+
+def test2():
+    person_1 = person()
+    person_1.from_floor = 3
+    person_1.to_floor = 12
+    person_1.come_time = 1
+    person_1.current_floor = 3
+    person_1.in_which_elevator = 0
+    person_1.is_out = False
+
+    person_2 = person()
+    person_2.from_floor = 3
+    person_2.to_floor = 12
+    person_2.come_time = 1
+    person_2.current_floor = 3
+    person_2.in_which_elevator = 0
+    person_2.is_out = False
+
+    person_3 = person()
+    person_3.from_floor = 3
+    person_3.to_floor = 12
+    person_3.come_time = 1
+    person_3.current_floor = 3
+    person_3.in_which_elevator = 0
+    person_3.is_out = False
+
+    person_4 = person()
+    person_4.from_floor = 4
+    person_4.to_floor = 6
+    person_4.come_time = 6
+    person_4.current_floor = 3
+    person_4.in_which_elevator = 0
+    person_4.is_out = False
+
+    person_5 = person()
+    person_5.from_floor = 4
+    person_5.to_floor = 6
+    person_5.come_time = 6
+    person_5.current_floor = 3
+    person_5.in_which_elevator = 0
+    person_5.is_out = False
+
+    core_algorithm(7, [person_1, person_2, person_3, person_4, person_5])
+    # 有3个人在1.0秒出现于第3层(第0层指地下三层,第3层指1楼),要前往第12层.有2个人在6.0秒出现在第4层,要前往第6层.求第7.0秒时电梯和人员的位置
+
+    # 由第3层到第12层,只能使用电梯0
+    # 由第4层到第6层,使用电梯2
+
+    # 1.0秒到7.0秒,共6秒,电梯上移三层,由3层到6层(还没有考虑进入电梯的时间)
+    # 6.0秒到7.0秒,共1秒,电梯上移0.5层,由3层到3.5层
+
+
+# 第一秒：p1 p2 p3 到一楼 电梯开始上行
+# 第二秒：p1进电梯e2
+# 第三秒：p2进电梯e2
+# 第四秒：p3进电梯e2
+# 第五秒：电梯e2到3.5
+# 第六秒：电梯e2到4
+# 第七秒：p4 p5到二楼，p4进电梯e2
+
+    # 所以应该返回
+    person_1.current_floor = 4.0
+    person_1.in_which_elevator = 2
+    person_2.current_floor = 4.0
+    person_2.in_which_elevator = 2
+    person_3.current_floor = 4.0
+    person_3.in_which_elevator = 2
+    person_4.current_floor = 4.0
+    person_4.in_which_elevator = 2
+    person_5.current_floor = 4.0
+    person_4.in_which_elevator = 0
+
+    return [[elevator(3.0, 0), elevator(4.0, 1), elevator(3.0, 0)], [person_1, person_2, person_3, person_4, person_5]]
+
+
+def test3():
+    person_1 = person()
+    person_1.from_floor = 3
+    person_1.to_floor = 12
+    person_1.come_time = 1
+    person_1.current_floor = 3.0
+    person_1.in_which_elevator = 0
+    person_1.is_out = False
+
+    person_2 = person()
+    person_2.from_floor = 3
+    person_2.to_floor = 12
+    person_2.come_time = 1
+    person_2.current_floor = 3.0
+    person_2.in_which_elevator = 0
+    person_2.is_out = False
+
+    person_3 = person()
+    person_3.from_floor = 3
+    person_3.to_floor = 12
+    person_3.come_time = 1
+    person_3.current_floor = 3.0
+    person_3.in_which_elevator = 0
+    person_3.is_out = False
+
+    person_4 = person()
+    person_4.from_floor = 4
+    person_4.to_floor = 6
+    person_4.come_time = 6
+    person_4.current_floor = 4.0
+    person_4.in_which_elevator = 0
+    person_4.is_out = False
+
+    person_5 = person()
+    person_5.from_floor = 4
+    person_5.to_floor = 6
+    person_5.come_time = 6
+    person_5.current_floor = 4.0
+    person_5.in_which_elevator = 0
+    person_5.is_out = False
+
+    return algorithm_implement.core_algorithm(7, [person_1, person_2, person_3, person_4, person_5])
+
+
+def py_test():
+    print('test2_is')
+    result2 = test2()
+    r2 = algorithm_result()
+    r2.elevators = result2[0]
+    r2.people = result2[1]
+    r2_json = json.dumps(r2, indent=4)
+    print(r2_json)
+
+    print('test3_is')
+    result3 = test3()
+    r3 = algorithm_result()
+    r3.elevators = result3[0]
+    r3.people = result3[1]
+    r3_json = json.dumps(r3, indent=4)
+    print(r3_json)
+
+    if r2_json == r3_json:
+        print('test success')
+    else:
+        print('test failed')
+
+
+if __name__ == "__main__":
+    path_json_files = os.listdir('test_json')
+    for path in path_json_files:
+        if 'in' in path:
+            print(path, end=' ')
+    print()
+    file_name = input('please input test file name like 1\n')
+    path_json = f'test_json/in{file_name}.json'
+    print(path_json)
+
+    js = file_util.read_all_text(path_json)
+    js = json.loads(js)
+    t = js['Time']
+
+    people = []
+    for p in js['People']:
+        person = algorithm_implement.person()
+        person.come_time = p['come_time']
+        person.from_floor = p['from_floor']
+        person.to_floor = p['to_floor']
+        person.current_floor = p['current_floor']
+        person.in_which_elevator = p['in_which_elevator']
+        person.is_out = p['is_out']
+
+        people.append(person)
+    result = algorithm_implement.core_algorithm(t, people)
+    r = algorithm_result()
+    r.elevators = result[0]
+    r.people = result[1]
+    r_json = json.dumps(r, indent=4)
+    print(r_json)
+
+    path_out_json = path_json.replace('in', 'out')
+    if os.path.exists(path_out_json):
+        print(f'find {path_out_json}')
+        out_json = file_util.read_all_text(path_out_json)
+        if out_json == r_json:
+            print('test success')
+        else:
+            print('test failed')
